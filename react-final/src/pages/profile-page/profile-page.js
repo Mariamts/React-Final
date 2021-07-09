bd18a94f706ab84467208925861db33b2fbbe7df
Mariam Tsotsolashvili <mariam.tsotsolashvili@geolab.edu.ge>
2021-07-09 11:28:47 +0400

    add logout

--- react-final/src/pages/profile-page/profile-page.js
+++ react-final/src/pages/profile-page/profile-page.js
@@ -1,11 +1,23 @@
+import { useHistory } from 'react-router';
 import Posts from '../../components/posts';
 import { withAuthProtected } from '../../hoc';
-import { PROFILE_PATH } from '../../utils/routePaths';
+import { logOut } from '../../services';
+import { AUTH_PATH, PROFILE_PATH } from '../../utils/routePaths';
 
 function ProfilePage() {
+  const history = useHistory();
+
+  const logout = () => {
+    logOut();
+    history.replace(AUTH_PATH);
+  };
+
   return (
     <div>
       <h2>Profile</h2>
+      <button className="btn btn-primary" onClick={() => logout()}>
+        logout
+      </button>
       <Posts type={PROFILE_PATH} />
     </div>
   );
